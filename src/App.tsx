1dacdcc64a5537c5a59363ce8526f65d392f63c2
Krish <68506145+ikrishagarwal@users.noreply.github.com>
2024-06-28 20:07:18 +0530

    feat: made leaderboard and ui fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,16 @@ import { LeaderBoard } from "./components/leaderboard";
 function App() {
   return (
     <>
-      <section className="min-h-screen bg-kiwi-900 overflow-hidden h-screen">
-        <Header></Header>
-        <section className="grid grid-cols-2 gap-16">
+      <section className="min-h-screen bg-kiwi-900 h-screen grid grid-rows-12 gap-16">
+        <section className="grid-row-span-1">
+          <Header></Header>
+        </section>
+        <section className="grid grid-cols-2 gap-16 grid-row-span-11 h-full">
           <Quiz
             question="What is the square root of 169?"
             options={["7", "13", "18", "17"]}
           ></Quiz>
-          <section>
+          <section className="h-[75%]">
             <Stats
               stats={{
                 username: "John Doe",
---
+++ src/components/leaderboard.tsx
@@ -0,0 +1,69 @@
+import { Pill } from "./elements/pill";
+
+export const LeaderBoard = ({}) => {
+  return (
+    <section className="bg-kiwi-600 rounded-xl p-8 m-8 h-[60%]">
+      <section className="flex justify-center">
+        <Pill content="LeaderBoard"></Pill>
+      </section>
+      <section className="mt-6 h-full text-white tracking-wider leading-relaxed flex justify-center">
+        <section className="w-fit h-auto max-h-[100%] overflow-scroll scrollbar">
+          <table className="table-auto leaderboard-table">
+            <thead className="border-b-2 border-kiwi-100">
+              <tr>
+                <th>Name</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Malcolm Lockyer</td>
+                <td>9/10</td>
+              </tr>
+              <tr>
+                <td>John Doe</td>
+                <td>5/10</td>
+              </tr>
+              <tr>
+                <td>Michael Hoffmann</td>
+                <td>7/10</td>
+              </tr>
+              <tr>
+                <td>Malcolm Lockyer</td>
+                <td>9/10</td>
+              </tr>
+              <tr>
+                <td>John Doe</td>
+                <td>5/10</td>
+              </tr>
+              <tr>
+                <td>Michael Hoffmann</td>
+                <td>7/10</td>
+              </tr>
+              <tr>
+                <td>David Smith</td>
+                <td>8/10</td>
+              </tr>
+              <tr>
+                <td>Malcolm Lockyer</td>
+                <td>9/10</td>
+              </tr>
+              <tr>
+                <td>John Doe</td>
+                <td>5/10</td>
+              </tr>
+              <tr>
+                <td>Michael Hoffmann</td>
+                <td>7/10</td>
+              </tr>
+              <tr>
+                <td>David Smith</td>
+                <td>8/10</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </section>
+    </section>
+  );
+};
