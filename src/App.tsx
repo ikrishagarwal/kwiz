9e89fe958bb9dc0aec2a57fe90d63f81a05a74e2
Krish <68506145+ikrishagarwal@users.noreply.github.com>
2024-06-28 18:57:23 +0530

    fix: code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <Header></Header>
         <section className="grid grid-cols-2 gap-16">
           <Quiz
-            question="What is the square root of 169? lorem ipsdflas jum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            question="What is the square root of 169?"
             options={["7", "13", "18", "17"]}
           ></Quiz>
           <section>
