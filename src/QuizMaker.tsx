8080994ab938beead518ae2e8417c4b543f0ce87
Krish <68506145+ikrishagarwal@users.noreply.github.com>
2024-06-30 18:28:48 +0530

    feat: completes quiz maker page

--- src/QuizMaker.tsx
+++ src/QuizMaker.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { LeaderBoard } from "./components/leaderboard";
 
 export const QuizMaker = ({}) => {
+  const [question, setQuestion] = useState("");
+  const [optionA, setOptionA] = useState("");
+  const [optionB, setOptionB] = useState("");
+  const [optionC, setOptionC] = useState("");
+  const [optionD, setOptionD] = useState("");
+
   return (
     <>
       <section className="w-full flex justify-center">
@@ -11,6 +18,23 @@ export const QuizMaker = ({}) => {
                 action="#"
                 onSubmit={(event) => {
                   event.preventDefault();
+
+                  if (question.trim() === "")
+                    return alert("Question can't be empty.");
+                  if (
+                    !optionA.trim() ||
+                    !optionB.trim() ||
+                    !optionC.trim() ||
+                    !optionD.trim()
+                  )
+                    return alert("You have to provide all four options.");
+
+                  const options = [optionA, optionB, optionC, optionD];
+
+                  console.log({
+                    question,
+                    options,
+                  });
                 }}
                 className="text-white"
               >
@@ -21,6 +45,8 @@ export const QuizMaker = ({}) => {
                     className="rounded-lg w-full bg-kiwi-200 text-slate-200 px-6 py-3 font-semibold outline-kiwi-100 focus:outline-1 focus:shadow-none scrollbar"
                     placeholder="ex: What's the square root of 169?"
                     rows={4}
+                    value={question}
+                    onChange={(event) => setQuestion(event.target.value)}
                   />
                 </section>
                 <section className="py-4">
@@ -30,21 +56,29 @@ export const QuizMaker = ({}) => {
                       className="rounded-lg bg-kiwi-200 my-2 text-slate-200 px-6 py-3 font-semibold outline-kiwi-100 focus:outline-1 focus:shadow-none"
                       type="text"
                       placeholder="Option A"
+                      value={optionA}
+                      onChange={(event) => setOptionA(event.target.value)}
                     />
                     <input
                       className="rounded-lg bg-kiwi-200 my-2 text-slate-200 px-6 py-3 font-semibold outline-kiwi-100 focus:outline-1 focus:shadow-none"
                       type="text"
                       placeholder="Option B"
+                      value={optionB}
+                      onChange={(event) => setOptionB(event.target.value)}
                     />
                     <input
                       className="rounded-lg bg-kiwi-200 my-2 text-slate-200 px-6 py-3 font-semibold outline-kiwi-100 focus:outline-1 focus:shadow-none"
                       type="text"
                       placeholder="Option C"
+                      value={optionC}
+                      onChange={(event) => setOptionC(event.target.value)}
                     />
                     <input
                       className="rounded-lg bg-kiwi-200 my-2 text-slate-200 px-6 py-3 font-semibold outline-kiwi-100 focus:outline-1 focus:shadow-none"
                       type="text"
                       placeholder="Option D"
+                      value={optionD}
+                      onChange={(event) => setOptionD(event.target.value)}
                     />
                   </section>
                 </section>
